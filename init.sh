57590a3723e4ac2a459952c45db387524a920f48
davidbutler000123 <davidbutler000123@gmail.com>
2025-01-06 19:42:33 +0100

    update generate.js

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 KEY="mykey"
-KEY1="mykey1"
 CHAINID="epix_4243-1"
 MONIKER="localtestnet"
 KEYRING="test"
@@ -24,7 +23,6 @@ epixd config set client keyring-backend $KEYRING
 
 # if $KEY exists it should be deleted
 epixd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO
-epixd keys add $KEY1 --keyring-backend $KEYRING --algo $KEYALGO
 
 # Set moniker and chain-id for epix (Moniker can be anything, chain-id must be an integer)
 epixd init $MONIKER --chain-id $CHAINID
@@ -42,23 +40,6 @@ cat $HOME/.epixd/config/genesis.json | jq '.app_state["inflation"]["params"]["mi
 # Set gas limit in genesis
 cat $HOME/.epixd/config/genesis.json | jq '.consensus["params"]["block"]["max_gas"]="10000000"' > $HOME/.epixd/config/tmp_genesis.json && mv $HOME/.epixd/config/tmp_genesis.json $HOME/.epixd/config/genesis.json
 
-# Set claims start time
-# node_address=$(epixd keys list | grep  "address: " | cut -c12-)
-# current_date=$(date -u +"%Y-%m-%dT%TZ")
-# cat $HOME/.epixd/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["airdrop_start_time"]=$current_date' > $HOME/.epixd/config/tmp_genesis.json && mv $HOME/.epixd/config/tmp_genesis.json $HOME/.epixd/config/genesis.json
-
-# # Set claims records for validator account
-# amount_to_claim=10000
-# cat $HOME/.epixd/config/genesis.json | jq -r --arg node_address "$node_address" --arg amount_to_claim "$amount_to_claim" '.app_state["claims"]["claims_records"]=[{"initial_claimable_amount":$amount_to_claim, "actions_completed":[false, false, false, false],"address":$node_address}]' > $HOME/.epixd/config/tmp_genesis.json && mv $HOME/.epixd/config/tmp_genesis.json $HOME/.epixd/config/genesis.json
-
-# # Set claims decay
-# cat $HOME/.epixd/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["duration_of_decay"]="1000000s"' > $HOME/.epixd/config/tmp_genesis.json && mv $HOME/.epixd/config/tmp_genesis.json $HOME/.epixd/config/genesis.json
-# cat $HOME/.epixd/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["duration_until_decay"]="100000s"' > $HOME/.epixd/config/tmp_genesis.json && mv $HOME/.epixd/config/tmp_genesis.json $HOME/.epixd/config/genesis.json
-
-# # Claim module account:
-# # 0xA61808Fe40fEb8B3433778BBC2ecECCAA47c8c47 || epix15cvq3ljql6utxseh0zau9m8ve2j8erz84vyscz
-# cat $HOME/.epixd/config/genesis.json | jq -r --arg amount_to_claim "$amount_to_claim" '.app_state["bank"]["balances"] += [{"address":"epix15cvq3ljql6utxseh0zau9m8ve2j8erz84vyscz","coins":[{"denom":"aepix", "amount":$amount_to_claim}]}]' > $HOME/.epixd/config/tmp_genesis.json && mv $HOME/.epixd/config/tmp_genesis.json $HOME/.epixd/config/genesis.json
-
 # disable produce empty block
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.epixd/config/config.toml
@@ -67,52 +48,55 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 if [[ $1 == "pending" ]]; then
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-      sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.epixd/config/config.toml
-      sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.epixd/config/config.toml
-  else
-      sed -i 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.epixd/config/config.toml
-      sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.epixd/config/config.toml
-  fi
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.epixd/config/config.toml
+        sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.epixd/config/config.toml
+    else
+        sed -i 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.epixd/config/config.toml
+        sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.epixd/config/config.toml
+    fi
 fi
 
-# Allocate genesis accounts (cosmos formatted addresses)
-epixd add-genesis-account $KEY 100000000000000000000010000aepix --keyring-backend $KEYRING
-epixd add-genesis-account $KEY1 100000000000000000000000000aepix --keyring-backend $KEYRING
 
-# Update total supply with claim values
-validators_supply=$(cat $HOME/.epixd/config/genesis.json | jq -r '.app_state["bank"]["supply"][0]["amount"]')
-# Bc is required to add this big numbers
-# total_supply=$(bc <<< "$amount_to_claim+$validators_supply")
-total_supply=200000000000000000000010000
+epixd add-genesis-account $KEY 23668257824195825000000000aepix --keyring-backend $KEYRING
+
+epixd add-genesis-account epix1cttyg9c0e72wcyg4fulxn5pajafvqerfvzg6ta 250123450000000000000aepix
+epixd add-genesis-account epix1shyv0p05z3dhtjr28w37qkcm54yg5ulnhzdwc4 21031052341866000000000aepix
+epixd add-genesis-account epix1fw4t8peek96a6x6a32v30y22l59ph5wc4hmqmw 12310000000000aepix
+
+        # Update total supply with claim values
+        validators_supply=$(cat $HOME/.epixd/config/genesis.json | jq -r '.app_state["bank"]["supply"][0]["amount"]')
+        total_supply=23689539000000001000000000
 cat $HOME/.epixd/config/genesis.json | jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' > $HOME/.epixd/config/tmp_genesis.json && mv $HOME/.epixd/config/tmp_genesis.json $HOME/.epixd/config/genesis.json
 
-# Sign genesis transaction
-epixd gentx $KEY 1000000000000000000000aepix --keyring-backend $KEYRING --chain-id $CHAINID
-
-# Collect genesis tx
-epixd collect-gentxs
-
-# Run this to ensure everything worked and that the genesis file is setup correctly
-epixd validate-genesis
-
-if [[ $1 == "pending" ]]; then
-  echo "pending mode is on, please wait for the first block committed."
-fi
-
-# Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-epixd start --pruning=nothing $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001aepix --json-rpc.api eth,txpool,personal,net,debug,web3 --rpc.laddr "tcp://0.0.0.0:26657" --api.enable --chain-id $CHAINID
+        # Sign genesis transaction
+        epixd gentx $KEY 1000000000000000000aepix --keyring-backend $KEYRING --chain-id $CHAINID
+        
+        # Collect genesis tx
+        epixd collect-gentxs
+        
+        # Run this to ensure everything worked and that the genesis file is setup correctly
+        epixd validate-genesis
+        
+        if [[ $1 == "pending" ]]; then
+          echo "pending mode is on, please wait for the first block committed."
+        fi
+        
+        # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+        epixd start --pruning=nothing $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001aepix --json-rpc.api eth,txpool,personal,net,debug,web3 --rpc.laddr "tcp://0.0.0.0:26657" --api.enable --chain-id $CHAINID
+        epixd tx distribution fund-community-pool 23668256824195825000000000aepix --from $KEY --gas-prices 100aepix  --keyring-backend $KEYRING --chain-id $CHAINID
+        
